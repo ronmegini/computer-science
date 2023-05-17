99f80576a2b84e67e4d0babcc59fef2aff2d6880
ron34353435 <ron34353435@gmail.com>
2023-05-17 17:15:47 +0300

    algo ex 1

--- algo/ex-1/main.py
+++ algo/ex-1/main.py
@@ -35,7 +35,7 @@ def prim(graph, root):
     return path
 
 
-def update_mst(graph, mst, removed_edge):
+def removeEdgeMST(graph, mst, removed_edge):
     
     # If removed edge not in the MST tree - return the same graph
     firstRemovedVertex, secondRemovedVertex = removed_edge
@@ -59,31 +59,32 @@ def update_mst(graph, mst, removed_edge):
     else:
         CopyOriginalGraph[secondRemovedVertex].pop(firstRemovedVertex)
 
-    # Create new copies of the updated MST
-    t1 = updatedMST.copy()
-    t2 = updatedMST.copy()
+    # Copy sub trees
+    T1 = updatedMST.copy()
+    T2 = updatedMST.copy()
 
-    # Return lists of reachable nodes from each graph
-    blueNodes = bfs(t1, firstRemovedVertex)
-    redNodes = bfs(t2, secondRemovedVertex)
+    # Use bfs to get reachable nodes
+    blueNodes = bfs(T1, firstRemovedVertex)
+    redNodes = bfs(T2, secondRemovedVertex)
 
-    # Initialize variables to find the connecting edge between blue & red nodes
+    # Set the minimum weight to infinity
     minWeight = float('inf')
+
     newfirstVertex = None
     newSecondVertex = None 
 
+    # Inerate over all the edges in the graph
     for edge in CopyOriginalGraph:
         for vertex in CopyOriginalGraph[edge]:
             firstVertex = edge
             secondVertex = vertex
             weight = CopyOriginalGraph[edge][vertex]
-            # Check if one node connects blue & red or the other way
+            # Is there any connected edge between blue and red vertexes
             if (firstVertex in blueNodes and secondVertex in redNodes) or (secondVertex in blueNodes and firstVertex in redNodes):
-                # Check if the weight is minimal and update
-                    if weight < minWeight:
-                        newfirstVertex = firstVertex
-                        newSecondVertex = secondVertex
-                        minWeight = weight
+                if weight < minWeight:
+                    newfirstVertex = firstVertex
+                    newSecondVertex = secondVertex
+                    minWeight = weight
 
     # Check if the graph is connected
     if newfirstVertex and newSecondVertex:
@@ -92,25 +93,23 @@ def update_mst(graph, mst, removed_edge):
 
         return updatedMST
     else:
-        print("The removed edge leads to a diconnected graph")
+        print("Alert - Disconnected Graph")
 
 
-def bfs(graph, node):
-  # Run BFS algorithm
-  visited = []
-  queue = []
-  visited.append(node)
-  queue.append(node)
+def bfs(graph, start_node):
+    visited = []
+    queue = [start_node]
 
-  while queue:
-    m = queue.pop(0)
+    while queue:
+        current_node = queue.pop(0)
+        visited.append(current_node)
 
-    for neighbour in graph[m]:
-      if neighbour not in visited:
-        visited.append(neighbour)
-        queue.append(neighbour)
+        for neighbor in graph[current_node]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.append(neighbor)
 
-  return visited
+    return visited
 
 if __name__ == '__main__':
 
@@ -129,18 +128,44 @@ if __name__ == '__main__':
         "L": {"K": 12, "H": 8, "F": 20}
     }
 
+    graph = {
+        "A": {"B": 12, "C": 23, "D": 5},
+        "B": {"A": 12, "F": 7, "M": 9},
+        "C": {"A": 23, "D": 18, "E": 17},
+        "D": {"A": 5, "C": 18, "G": 9, "F": 10},
+        "E": {"C": 17, "I": 16, "J": 14, "R": 3},
+        "F": {"B": 7, "L": 20, "D": 10},
+        "G": {"D": 9, "H": 4, "J": 3},
+        "H": {"G": 4, "L": 8},
+        "I": {"E": 16, "K": 7},
+        "J": {"G": 3, "E": 14},
+        "K": {"I": 7, "L": 12},
+        "L": {"K": 12, "H": 8, "F": 20},
+        "M": {"B": 9, "N": 13},
+        "N": {"M": 13, "O": 11},
+        "O": {"N": 11, "P": 6},
+        "P": {"O": 6, "Q": 15},
+        "Q": {"P": 15, "R": 19},
+        "R": {"Q": 19, "S": 2, "E": 3},
+        "S": {"R": 2, "T": 21},
+        "T": {"S": 21, "U": 1},
+        "U": {"T": 1, "V": 22},
+        "V": {"U": 22, "W": 24},
+        "W": {"V": 24, "X": 25},
+        "X": {"W": 25, "Y": 26},
+        "Y": {"X": 26, "Z": 27},
+        "Z": {"Y": 27}
+    }
+
     # Ex 1
-
     print("Original Graph: ")
-    print(originalGraph)
-
+    print(graph)
     print("MST Tree: ")
-    mst = prim(originalGraph, 'A')
+    mst = prim(graph, 'A')
     print(mst)
 
     # Ex 2
     print("Remove not in MST edge (A,C): ")
-    print(update_mst(originalGraph, mst, ("A", "C")))
-
+    print(removeEdgeMST(graph, mst, ("A", "C")))
     print("Remove edge (J,E): ")
-    print(update_mst(originalGraph, mst, ("J", "E")))
+    print(removeEdgeMST(graph, mst, ("J", "E")))
